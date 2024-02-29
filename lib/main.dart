51fa9da40275af8e70fc5758fdf34be3bb30afcf
miwatanaka1950 <146437142+miwatanaka1950@users.noreply.github.com>
2024-02-29 17:56:04 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -275,3 +275,96 @@ class _AnimatedProgressIndicatorState extends State<AnimatedProgressIndicator>
   }
 }
 
+//SHKAF!!!
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      
+      home: const HomePage(),
+    );
+  }
+}
+class HomePage extends StatelessWidget {
+const HomePage({super.key});
+@override
+Widget build(BuildContext context) {
+
+  return Scaffold(
+    body:  SafeArea(
+      bottom: false,
+      child: Center(
+        child: Column(
+
+          textDirection: TextDirection.ltr,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          verticalDirection: VerticalDirection.down,
+
+          children: <Widget>[
+            Expanded(
+              child:
+              Row(
+                children: <Widget>[
+                  Expanded(child:
+                  Container(
+                    color: Colors.black,
+                    margin: EdgeInsets.only(left: 8, top: 8, right: 4, bottom: 4),
+
+                  ),
+                  ),
+                  Expanded(child: Container(color: Colors.yellow, margin: EdgeInsets.only(left: 4, top: 8, right: 8, bottom: 4),)),
+                ],
+              ),
+              flex: 3,
+            ),
+            Expanded(
+                child: Row(
+                  children: [
+                    Expanded(child: Container(color: Colors.red, margin: EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 4),)),
+                    Expanded(child: Container(color: Colors.orange, margin: EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 4),)),
+                  ],
+                ),
+                flex:1
+            ),
+            Expanded(
+                child: Row(
+                  children: [
+                    Expanded(child: Container(color: Colors.indigoAccent, margin: EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 4),)),
+                    Expanded(child: Container(color: Colors.cyan, margin: EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 4),)),
+                  ],
+                ),
+                flex:1
+            ),
+            Expanded(
+              child:
+              Row(
+                children: <Widget>[
+                  Expanded(child: Container(color: Colors.teal, margin: EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 8),)),
+                  Expanded(child: Container(color: Colors.grey, margin: EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 8),)),
+                ],
+              ),
+              flex: 3,
+            ),
+          ],
+        ),
+      ),
+    ),
+
+  );
+}
+}
+
+//sait!!!
+
+
+
